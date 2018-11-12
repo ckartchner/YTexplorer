8c1b32dbe657db9053c7a4e91e75d88952401f1f
Carlin Kartchner <carlinsjunk2@gmail.com>
2018-11-11 20:51:43 -0500

    Refactor and cleanup after meetup
    Remove unused imports
    PEP8 cleanup
    Add sum_time function
    Refactor argparse -- add stats and id

--- getstats.py
+++ getstats.py
@@ -1,16 +1,31 @@
+"""
+Youtube API Parts:
+https://developers.google.com/youtube/v3/docs/videos/list
+
+TODO:
+get_ids_on_domain():
+get_ids_x_links_away():
+"""
+
 import requests
 import isodate
 import os
 from dotenv import load_dotenv  # installed with 'pip install python-dotenv'
 import argparse
-from bs4 import BeautifulSoup
 import datetime
-import requests
 import re
-# import
+
+
+def sum_time(website):
+    id_set = get_ids(website)
+    total_time = datetime.timedelta(0)
+    for video_id in id_set:
+        total_time += stats(video_id)
+    return total_time
+
 
 def get_ids(website):
-    '''Find all youtube video ids input page'''
+    """Find all youtube video ids input page"""
     html_page = requests.get(website)
     # TBD when this is ever hit...
     try:
@@ -24,44 +39,47 @@ def get_ids(website):
     regobj = re.compile(r'(?<=watch\?v=)(.){11}')
     matches = regobj.finditer(html_page.text)
     id_set = set([m.group(0) for m in matches])
-    tot_time = datetime.timedelta(0)
-    for videoid in id_set:
-        tot_time += stats(videoid)
-    print(id_set)
-    print(tot_time)
+    return id_set
 
-# Youtube API Parts:
-# https://developers.google.com/youtube/v3/docs/videos/list
 
-# Load api key from environment variable
 def stats(videoid):
+    # Load api key from environment variable
     load_dotenv(dotenv_path="./.env")
     apikey = os.environ.get('APIKEY')
 
-    #id = 'Ov_znn3KHfE'  # Unique id for video to look at
     payload = {
         'id': videoid,
-        'part': 'contentDetails',
+        'part': 'contentDetails, snippet',
         'key': apikey
     }
-    r = requests.get('https://content.googleapis.com/youtube/v3/videos', params = payload)
-    # print(r.json())
+    r = requests.get('https://content.googleapis.com/youtube/v3/videos', params=payload)
 
     # Get the time
     # Duration is ISO8601 format
     # https://developers.google.com/youtube/v3/docs/videos
 
     duration = r.json()['items'][0]['contentDetails']['duration']
+    title = r.json()['items'][0]['snippet']['title']
     pydur = isodate.parse_duration(duration)
 
     # Format duration as pretty string
     dur_str = str(pydur)
-    print(f'Video duration: {dur_str}')
+    print(f'Duration: {dur_str} || Title: {title}')
     return pydur
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--site', help='input the url website')
-    args = parser.parse_args()
 
-    get_ids(args.site)
+if __name__ == '__main__':
+    # Args for debug purposes
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--site', help='Specify website used to get statistics. Expects full url.')
+    parser.add_argument('--stats', help='Get stats for single youtube video id. Expects youtube id.')
+    parser.add_argument('--ids', help='Get all youtube ids on a single page. Expects full url.')
+    args = parser.parse_args()
+    if args.site is not None:
+        all_time = sum_time(args.site)
+        print(f'All videos time: {all_time}')
+    if args.ids is not None:
+        ids = get_ids(args.ids)
+        print(ids)
+    if args.stats is not None:
+        stats(args.stats)
